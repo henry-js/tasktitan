d03820baf5fe7ada1f8f24f2598ad479aa5aad20
henry-js <79054685+henry-js@users.noreply.github.com>
2024-05-01 23:13:47 +0100

    chore: add Release debug config

--- src/Cli/Constants.cs
+++ src/Cli/Constants.cs
@@ -2,5 +2,13 @@ namespace TaskTitan.Cli;
 
 public static class Constants
 {
-    public static readonly string ConfigurationDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".tasktitan");
+    static Constants()
+    {
+#if DEBUG
+        ConfigurationDirectory = "./";
+#else
+        ConfigurationDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".tasktitan");
+#endif
+    }
+    public static readonly string ConfigurationDirectory;
 }
