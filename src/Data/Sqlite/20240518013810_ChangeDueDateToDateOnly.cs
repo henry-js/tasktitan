b318db92fad37913ddf341d21a54458ac266c4d4
henry-js <79054685+henry-js@users.noreply.github.com>
2024-05-18 23:01:00 +0100

    chore: update tasksdb schema

---
+++ src/Data/Sqlite/20240518013810_ChangeDueDateToDateOnly.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Sqlite
+{
+    /// <inheritdoc />
+    public partial class ChangeDueDateToDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
