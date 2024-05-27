b47540633fa2cab38b88f8be4c930e90807b7d2c
henry-js <79054685+henry-js@users.noreply.github.com>
2024-05-27 03:23:48 +0100

    add Ttask DTO

--- src/Lib/Dates/IDateTimeConverter.cs
+++ src/Lib/Dates/IDateTimeConverter.cs
@@ -4,3 +4,7 @@ public interface IDateTimeConverter
 {
     public DateOnly? ConvertFrom(string value);
 }
+public interface IStringFilterConverter<T> where T : struct
+{
+    public DateTime? ConvertFrom(string value);
+}
